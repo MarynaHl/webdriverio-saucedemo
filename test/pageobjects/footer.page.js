0c914566ac17de0c5ee293ae35c31b22cee3a9ee
Maryna Hlushak <75501308+MarynaHl@users.noreply.github.com>
2025-01-24 12:50:59 +0000

    feat: unify social media logic into FooterPage (remove repetition)

--- test/pageobjects/footer.page.js
+++ test/pageobjects/footer.page.js
@@ -3,16 +3,35 @@ class FooterPage {
     get facebookLink() { return $('footer .social_facebook'); }
     get linkedinLink() { return $('footer .social_linkedin'); }
 
-    async clickTwitter() {
-        await this.twitterLink.click();
+    async openSocialLink(network) {
+        switch (network) {
+            case 'twitter':
+                await this.twitterLink.click();
+                await browser.pause(1500);
+                await browser.switchWindow('twitter.com');
+                break;
+            case 'facebook':
+                await this.facebookLink.click();
+                await browser.pause(1500);
+                await browser.switchWindow('facebook.com');
+                break;
+            case 'linkedin':
+                await this.linkedinLink.click();
+                await browser.pause(1500);
+                await browser.switchWindow('linkedin.com');
+                break;
+            default:
+                throw new Error(`Unknown network: ${network}`);
+        }
     }
 
-    async clickFacebook() {
-        await this.facebookLink.click();
+    async getCurrentUrl() {
+        return browser.getUrl();
     }
 
-    async clickLinkedIn() {
-        await this.linkedinLink.click();
+    async closeTabAndSwitchBack() {
+        await browser.closeWindow();
+        await browser.switchWindow('saucedemo.com');
     }
 }
 
