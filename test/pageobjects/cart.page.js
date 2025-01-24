c13310cdafaae6c2dd72f4b1f4b3823940f3ebd0
Maryna Hlushak <75501308+MarynaHl@users.noreply.github.com>
2025-01-24 12:49:11 +0000

    feat: update CartPage to handle item count within POM

--- test/pageobjects/cart.page.js
+++ test/pageobjects/cart.page.js
@@ -2,8 +2,13 @@ class CartPage {
     get cartItems()   { return $$('.cart_item'); }
     get checkoutBtn() { return $('#checkout'); }
 
+    async getItemsCount() {
+        const items = await this.cartItems;
+        return items.length;
+    }
+
     async isAnyProductInCart() {
-        return (await this.cartItems.length) > 0;
+        return (await this.getItemsCount()) > 0;
     }
 
     async clickCheckout() {
